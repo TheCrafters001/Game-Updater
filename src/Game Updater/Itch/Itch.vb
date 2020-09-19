3daf70c94972646da92ace6e114f460731e35a26
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-09-18 22:24:33 -0400

    Update Code

---
+++ src/Game Updater/Itch/Itch.vb
@@ -0,0 +1,3 @@
+﻿Public Class Itch
+
+End Class
--- src/Game Updater/Misc/Form1.vb
+++ src/Game Updater/Misc/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub about_pcbox_Click(sender As Object, e As EventArgs) Handles about_pcbox.Click
         About.Show()
     End Sub
+
+    Private Sub itch_pcbox_Click(sender As Object, e As EventArgs) Handles itch_pcbox.Click
+        ItchPush.ItchPush.FileCheck()
+    End Sub
 End Class
